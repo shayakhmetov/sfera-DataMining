a5f58156ac19481bca887c1b0bd3b76fbd07fb63
shayakhmetov <shayakhmetov.rim@gmail.com>
2014-12-21 01:24:38 +0300

    rsm added. wine dataset added.

--- homework3/GradientBoosting.py
+++ homework3/GradientBoosting.py
@@ -3,7 +3,7 @@ __author__ = 'rim'
 from scipy.optimize import minimize_scalar
 import numpy
 from sklearn.datasets import load_iris
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, ShuffleSplit
 from CART import CartTree
 import random
 from sklearn.metrics import accuracy_score
@@ -14,7 +14,7 @@ def log_e():
 
 
 class StochasticGradientBoosting():
-    def __init__(self, n_trees=2, min_leaf_size=5, rsm=0.2, bagging=1, regularization_coef=0.1):
+    def __init__(self, n_trees=2, min_leaf_size=5, rsm=1, bagging=0.2, regularization_coef=0.1):
         self.n_trees = n_trees
         self.min_leaf_size = min_leaf_size
         self.composition = []
@@ -27,22 +27,33 @@ class StochasticGradientBoosting():
     def fit(self, data_set, target_set):
         labels = numpy.unique(target_set)
         self.f0 = minimize_scalar(lambda gamma: self.__logistic_loss_function(target_set, gamma)).x
-        other_data_set, other_target_set = data_set, target_set
         current_data_set, current_target_set = data_set, target_set
+
         for m in range(self.n_trees):
-            if other_target_set.shape[0] < 2:
-                return None
             if self.bagging < 1.:
                 other_data_set, current_data_set, other_target_set, current_target_set =\
                     train_test_split(data_set, target_set, test_size=self.bagging)
+            if current_target_set.shape[0] == 0:
+                return None
+
+            feature_indices = numpy.array([i for i in range(data_set.shape[1])])
+            rsm_bag_data_set = current_data_set
+
+            if self.rsm < 1.:
+                split = ShuffleSplit(data_set.shape[1], test_size=self.rsm, n_iter=1)
+                for o_f, c_f in split:
+                    feature_indices = c_f
+                    feature_indices = numpy.sort(feature_indices)
+                rsm_bag_data_set = current_data_set[:, feature_indices]
+
             rm = - self.__gradient_loss_function(current_target_set, self.__decision_func(current_data_set))
             cart_tree = CartTree(self.min_leaf_size)
-            cart_tree.fit(current_data_set, rm)
+            cart_tree.fit(rsm_bag_data_set, rm)
             gamma_m = minimize_scalar(lambda gamma:
                                       self.__logistic_loss_function(current_target_set, self.__sigmoid(
                                           self.__decision_func(current_data_set) +
-                                          gamma*cart_tree.predict(current_data_set))), method='bounded', bounds=(-100, 100))
-            self.composition.append((self.regularization_coef*gamma_m.x, cart_tree))
+                                          gamma*cart_tree.predict(rsm_bag_data_set))), method='bounded', bounds=(-100, 100))
+            self.composition.append((self.regularization_coef*gamma_m.x, cart_tree, feature_indices))
 
     def __logistic_loss_function(self, y, fx):
         sig = self.__sigmoid(fx)
@@ -57,18 +68,18 @@ class StochasticGradientBoosting():
         return numpy.log2(numpy.exp(-y)) + self.__sigmoid(-fx)
 
 
-    def __decision_func(self, xs):
+    def __decision_func(self, xs, predict=False):
         decision = numpy.full(xs.shape[0], self.f0)
-        for b, tree in self.composition:
+        for b, tree, indices in self.composition:
             decision += b*tree.predict(xs)
         return decision
 
     def predict(self, xs):
-        return self.__sigmoid(self.__decision_func(xs))
+        return self.__sigmoid(self.__decision_func(xs, True))
 
     def print_b(self):
         print '{',
-        for b, tree in self.composition:
+        for b, tree, indices in self.composition:
             print b,
         print '}'
 
@@ -86,13 +97,24 @@ def construct_predicted(pd1, pd2, pd3):
         l = numpy.argmax([pd1[i], pd2[i], pd3[i]])
         yield l
 
+
+def load_wine():
+    xs = []
+    with open('wine.data', "r") as data_file:
+        for line in data_file:
+            list.insert(xs, 0, line.split(","))
+    targets = [int(line[0]) - 1 for line in xs]
+    xs = numpy.array(xs, dtype=float)
+    xs = xs[:, 1:]
+    return {'data': xs, 'target': targets}
+
 def main():
-    data_set = load_iris()['data']
-    target_set = load_iris()['target']
+    data_set = load_wine()['data']
+    target_set = load_wine()['target']
     min_n_trees = 1
-    max_n_trees = 50
-    min_leaf_size = 5
-    max_leaf_size = 6
+    max_n_trees = 100
+    min_leaf_size = 4
+    max_leaf_size = 5
 
     train_data_set, test_data_set, train_target_set, test_target_set = \
                 train_test_split(data_set, target_set, test_size=0.2)
